f824429a7bc8d97eb573f146115c3e9561876808
charlessonbo <66862592+charlessonbo@users.noreply.github.com>
2021-11-16 16:29:08 +0800

    add by map templates

--- weather/urls.py
+++ weather/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', 
+         views.index, 
+         name='index'),
+
+    path('/get/location/weather',
+         views.get_weather_by_coordinates, 
+         name='get_weather_by_coordinates')  
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
-    return render(request, 'weather/index.html')
+    return render(request, 'weather/bymaptemplates/index.html')
+
+
+def get_weather_by_coordinates(request):
+    print("HEREH RHEHRH HERH HERH")
+    response = {}
+    response['result'] = 'error'
+    response['message'] = 'Some error message'
+
+    return JsonResponse({"valid":True}, status = 200)
