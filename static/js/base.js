745fa52d2ffaaccc7f991bb522a7349b6449d9f3
charlessonbo <66862592+charlessonbo@users.noreply.github.com>
2021-11-16 14:27:30 +0800

    add weather forecast template

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    var lat, lng;
     var map = L.map('map').setView([0,0],1)
 
     L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=iokfUVv9rgeWKrI9Vng4', 
@@ -8,7 +8,15 @@ $(document).ready(function () {
     }).addTo(map)
   
 
-
+    map.on('click', function(e){
+        var lt = String(e.latlng.lat),
+        lg = String(e.latlng.lng);
+        var popup = L.popup()
+          .setLatLng(e.latlng)
+          .setContent(lt + " " + lg)
+          .openOn(map);
+      });
+     
 
 
 });
