ecc3c354696737c631ac0b5424875e7a71b0302c
charlessonbo <66862592+charlessonbo@users.noreply.github.com>
2021-11-16 13:11:57 +0800

    add location map leaflet js

--- static/js/base.js
+++ static/js/base.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
-    alert_message_fade()
+
+    var map = L.map('map').setView([0,0],1)
+
+    L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=iokfUVv9rgeWKrI9Vng4', 
+    { 
+        attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+    }).addTo(map)
+  
+
+
+
+
 });
 
-function alert_message_fade(){
-    $(".alert").fadeTo(5000, 500).slideUp(500, function(){
-        $(".alert").slideUp(5000);
-    });
-}
+
